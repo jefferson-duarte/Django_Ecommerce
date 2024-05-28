e0c2c4483c308586ee2314280894e176107e558c
Jefferson Duarte <jeffersonsduarte@live.com>
2024-05-28 10:13:58 +0100

    Created Pedido and ItemPedido models

--- loja/settings.py
+++ loja/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     "debug_toolbar",
     'produto',
+    'pedido',
 
 ]
 
--- pedido/admin.py
+++ pedido/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from .models import Pedido, ItemPedido
 
-# Register your models here.
+
+@admin.register(ItemPedido)
+class ItemPedidoAdmin(admin.ModelAdmin):
+    list_display = [
+        'pedido',
+        'produto',
+        'variacao',
+        'preco',
+        'quantidade',
+    ]
+
+
+class ItemPedidoInLine(admin.TabularInline):
+    model = ItemPedido
+    extra = 1
+
+
+@admin.register(Pedido)
+class PedidoAdmin(admin.ModelAdmin):
+    list_display = [
+        'usuario',
+        'total',
+        'status',
+    ]
+
+    inlines = [
+        ItemPedidoInLine
+    ]
