b30a3626cc20438c06d049aa7fd1d9065c58d7f4
Jefferson Duarte <jeffersonsduarte@live.com>
2024-05-28 10:46:03 +0100

    Created Perfil model

--- loja/settings.py
+++ loja/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "debug_toolbar",
     'produto',
     'pedido',
+    'perfil',
 
 ]
 
--- perfil/admin.py
+++ perfil/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Perfil
 
-# Register your models here.
+
+@admin.register(Perfil)
+class PerfilAdmin(admin.ModelAdmin):
+    list_display = [
+        'usuario',
+        'data_nascimento',
+        'idade',
+        'cpf',
+    ]
